910db72682eb606d094e4de4f49cfb2c3e62200b
vibeforge1111 <vibeforge1111@gmail.com>
2026-05-12 17:31:21 +0400

    Keep build E2E isolated from live route gate
    (cherry picked from commit 0a0cb5b1b1e1b31247eae0c36b52fe8babbc875c)

--- tests/buildE2E.test.ts
+++ tests/buildE2E.test.ts
@@ -816,6 +816,7 @@ async function run(): Promise<void> {
 		process.env.SPAWNER_UI_URL = 'http://stub-spawner.test';
 		process.env.SPAWNER_UI_PUBLIC_URL = 'http://stub-spawner.test';
 		process.env.SPARK_BUILDER_BRIDGE_MODE = 'off';
+		process.env.SPARK_BOT_TEST_MODE = '1';
 
 		const indexModule: any = await import('../src/index');
 		const prd = indexModule.buildDomainChipPrd('creates weird poster prompts from dream fragments');
